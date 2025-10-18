11bd4ee98df4fb1b62263df5a89dc55f1c24a1d8
Pedro Machado <ph374011@gmail.com>
2025-10-18 20:56:37 -0300

    Finished Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,108 @@
-import Image from "next/image";
+"use client"
+
+import { getWeatherData } from "@/actions/action";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { WeatherData } from "@/interface/weather";
+import { error } from "console";
+import { Droplets, LucideX, Search, Thermometer, Wind } from "lucide-react"
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
+import { easeInOut, motion } from "framer-motion"
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <Button type="submit" disabled={pending}>
+      <Search className={`h-4 w-4 ${pending ? "animate-spin" : ""}`} />
+    </Button>
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  const [weather, setWeather] = useState<WeatherData | null>(null)
+  const [error, setError] = useState<string>("")
+
+  const handleSearch = async (formData: FormData) => {
+    setError("")
+
+    const city = formData.get("city") as string
+    const { data, error: weatherError } = await getWeatherData(city)
+    console.log(error)
+
+    if (weatherError) {
+      setError(weatherError)
+      setWeather(null)
+    }
+
+    if (data) {
+      setWeather(data)
+    }
+  }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-sky-950 to-gray-900 p-4 flex items-center justify-center">
+      <div className="w-full max-w-md space-y-4">
+        <form action={handleSearch} className="flex gap-2">
+          <Input
+            name="city"
+            type="text"
+            placeholder="enter city name..."
+            className="bg-white/90 "
+            required
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <SubmitButton />
+        </form>
+
+        {error && (
+          <motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0 }} className="text-center text-red-200 bg-red-500/20 rounded-md border-1 border-red-700 p-2 text-2xl">
+            {error}
+            <LucideX className="m-auto mt-1 size-24" />
+          </motion.div>
+        )}
+
+        {weather && (
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0 }} transition={{ duration: 1, ease: "easeInOut" }} >
+            <Card className="bg-gray-700 backdrop-blur">
+              <CardContent className="p-6">
+                <div>
+                  <h2 className="text-4xl bg-amber-50 text-center rounded-2xl p-2 m-auto">{weather.name}</h2>
+                  <div className="flex items-center justify-center gap-2 mt-2">
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt={weather.weather[0].description}
+                      width={80}
+                      height={80}
+                    />
+                    <div><p className="text-white text-2xl bold">{Math.round(weather.main.temp)}°C</p></div>
+                  </div>
+                  <div><h3 className="text-gray-400 text-center capitalize">{weather.weather[0].description}</h3></div>
+                </div>
+                <div className="grid grid-cols-3 gap-4 mt-6">
+                  <div className="text-center">
+                    <Thermometer className="w-6 h-6 mx-auto text-orange-500" />
+                    <div className="mt-2 text-sm text-gray-300">feels like</div>
+                    <div className="font-semibold text-gray-200">{Math.round(weather.main.feels_like)}°C</div>
+                  </div>
+                  <div className="text-center">
+                    <Droplets className="w-6 h-6 mx-auto text-blue-500" />
+                    <div className="mt-2 text-sm text-gray-300">Humiditty</div>
+                    <div className="font-semibold text-gray-200">{Math.round(weather.main.humidity)}%</div>
+                  </div>
+                  <div className="text-center">
+                    <Wind className="w-6 h-6 mx-auto text-white" />
+                    <div className="mt-2 text-sm text-gray-300">Wind</div>
+                    <div className="font-semibold text-gray-200">{Math.round(weather.main.feels_like)} m/s</div>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </motion.div>
+        )}
+      </div>
     </div>
   );
 }
